d4c3626c1a20c05dac5e4fa4b4364bd3708cc7f7
Jdjohar <Jdeep514@gmail.com>
2025-10-25 13:37:28 +0530

    new content

--- client/src/screens/userpanel/Edititem.jsx
+++ client/src/screens/userpanel/Edititem.jsx
@@ -33,7 +33,7 @@ export default function Edititem() {
     const fetchitemData = async () => {
         try {
             const authToken = localStorage.getItem('authToken');
-            const response = await fetch(`http://localhost:3001/api/getitems/${itemId}`, {
+            const response = await fetch(`https://immaculate.onrender.com/api/getitems/${itemId}`, {
                 headers: {
                   'Authorization': authToken,
                 }
